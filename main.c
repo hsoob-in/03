699dc370f954b4543c8a6f002cb932e9acf7b997
hsoob-in <tnqls2361@sookmyung.ac.kr>
2025-09-21 12:03:18 +0900

    실습3

--- main.c
+++ main.c
@@ -2,13 +2,14 @@
 
 int main(void) {
    char c;
-   int i;
+   char d;
    
-   printf("input a number: ");
+   printf("Enter a character : ");
    scanf("%c", &c);
    
-   i = c-'0';
-   printf("The input number is %i\n", i);
+   d = c + 1;
+   
+   printf("The next character of %c (%d) is %c (%d)\n", c,c,d,d);
    
    system("PAUSE");
    
