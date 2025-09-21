49162749e867637f95b59dd8f86e52684cfe6772
hsoob-in <tnqls2361@sookmyung.ac.kr>
2025-09-21 11:46:15 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 int main(void) {
-    int input_int;
-    float input_float;
-    
-    printf("enter an integer : ");
-    scanf("%d", &input_int);
-    
-    printf("enter a float : ");
-    scanf("%f", &input_float);
-    
-    printf("integer : %d, float : %f\n", input_int, input_float);
-    system("PAUSE");
-    
-    return 0;
+   char c;
+   int i;
+   
+   printf("input a number: ");
+   scanf("%c", &c);
+   
+   i = c-'0';
+   printf("The input number is %i\n", i);
+   
+   system("PAUSE");
+   
+   return 0;
 }
